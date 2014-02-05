01a8db6ac77df7ea263267f0a42dc8675af68618
Jamie Turner <jamieturner84@hotmail.com>
2014-02-05 21:27:24 +0000

    Added mandrill settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,12 @@ RailsStripeMembershipSaas::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 25,
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password  => ENV["MANDRILL_API_KEY"]
+}
 
 
 
