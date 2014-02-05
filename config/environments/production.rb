277c27f56eb5a909f74d0c88cd8c4d651b935fbe
Jamie Turner <jamieturner84@hotmail.com>
2014-02-05 22:30:15 +0000

    fix reset email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ RailsStripeMembershipSaas::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'http://www.clockworkflies.com' }
+  config.action_mailer.default_url_options = { :host => 'www.clockworkflies.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
