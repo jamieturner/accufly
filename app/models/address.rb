7fa6c03f016bf1fdc3c29b0a7f128b3877830c4d
Jamie Turner <jamieturner84@hotmail.com>
2014-02-05 22:26:33 +0000

    Fixed address required issue Styled forgot password screen Fixed password reset link

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ActiveRecord::Base
   belongs_to :user
   attr_accessible :country, :county, :first_line, :postcode, :second_line, :town_city, :user_id
-  #validates :country, :user_id, :county, :first_line, :postcode, :town_city, presence: true
+  validates :first_line, :town_city, :county, :postcode, :country, presence: true
 end
